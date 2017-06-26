f551411060a18c8a2b3761f5626974ac678a14d4
NAKAGAKI Kenji <nakaken0629@gmail.com>
2017-06-26 23:37:24 +0900

    連続投入の制限処理を追加

--- Assets/Scripts/CandyDestroyer.cs
+++ Assets/Scripts/CandyDestroyer.cs
@@ -4,8 +4,14 @@ using UnityEngine;
 
 public class CandyDestroyer : MonoBehaviour
 {
+	public CandyHolder candyHolder;
+	public int reward;
+
 	void OnTriggerEnter (Collider other)
 	{
+		// 指定数だけCandyのストックを増やす
+		candyHolder.AddCandy (reward);
+
 		if (other.gameObject.tag == "Candy") {
 			// オブジェクトを削除
 			Destroy (other.gameObject);
--- Assets/Scripts/Shooter.cs
+++ Assets/Scripts/Shooter.cs
@@ -5,12 +5,15 @@ using UnityEngine;
 public class Shooter : MonoBehaviour
 {
 	const int SphereCandyFrequency = 3;
+	const int MaxShotPower = 5;
+	const int RecoverySeconds = 3;
 
 	int sampleCandyCount;
+	int shotPower = MaxShotPower;
 
 	public GameObject[] candyPrefabs;
 	public GameObject[] candySquarePrefabs;
-	public GameObject candyHolder;
+	public CandyHolder candyHolder;
 	public float shotSpeed;
 	public float shotTorque;
 	public float baseWidth;
@@ -50,6 +53,14 @@ public class Shooter : MonoBehaviour
 
 	public void Shot ()
 	{
+		// キャンディを生成できる条件外だったらShotしない
+		if (candyHolder.GetCandyAmount () <= 0) {
+			return;
+		}
+		if (shotPower <= 0) {
+			return;
+		}
+
 		// プレファブからCandyオブジェクトを生成
 		GameObject candy = (GameObject)Instantiate (
 			                   SampleCandy (),
@@ -64,5 +75,37 @@ public class Shooter : MonoBehaviour
 		Rigidbody candyRigidbody = candy.GetComponent<Rigidbody> ();
 		candyRigidbody.AddForce (transform.forward * shotSpeed);
 		candyRigidbody.AddForce (new Vector3 (0, shotTorque, 0));
+
+		// Candyのストックを消費
+		candyHolder.ConsumeCandy ();
+		// ShotPowerを消費
+		ConsumePower ();
+	}
+
+	void OnGUI ()
+	{
+		GUI.color = Color.black;
+
+		// ShotPowerの残数を+の数で表示
+		string label = "";
+		for (int i = 0; i < shotPower; i++) {
+			label = label + "+";
+		}
+
+		GUI.Label (new Rect (0, 15, 100, 30), label);
+	}
+
+	void ConsumePower ()
+	{
+		// ShotPowerを消費すると同時に回復のカウントをスタート
+		shotPower--;
+		StartCoroutine (RecoverPower ());
+	}
+
+	IEnumerator RecoverPower ()
+	{
+		// 一定秒数待った後にshotPowerを回復
+		yield return new WaitForSeconds (RecoverySeconds);
+		shotPower++;
 	}
 }
