7c3a2cf29df5895ea98a3b2eefe641457474d004
NAKAGAKI Kenji <nakaken0629@gmail.com>
2017-06-29 00:59:45 +0900

    パーティクルの追加

--- Assets/Scripts/CandyDestroyer.cs
+++ Assets/Scripts/CandyDestroyer.cs
@@ -6,15 +6,26 @@ public class CandyDestroyer : MonoBehaviour
 {
 	public CandyHolder candyHolder;
 	public int reward;
+	public GameObject effectPrefab;
+	public Vector3 effectRotation;
 
 	void OnTriggerEnter (Collider other)
 	{
-		// 指定数だけCandyのストックを増やす
-		candyHolder.AddCandy (reward);
-
 		if (other.gameObject.tag == "Candy") {
+			// 指定数だけCandyのストックを増やす
+			candyHolder.AddCandy (reward);
+
 			// オブジェクトを削除
 			Destroy (other.gameObject);
+
+			if (effectPrefab != null) {
+				// Candyのポジションにエフェクトを生成
+				Instantiate (
+					effectPrefab,
+					other.transform.position,
+					Quaternion.Euler (effectRotation)
+				);
+			}
 		}
 	}
 }
